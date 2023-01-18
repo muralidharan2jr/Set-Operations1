f2cb3a1eb108b5cf92a0f48ca50d1006bc894f6d
muralidharan2jr <114759278+muralidharan2jr@users.noreply.github.com>
2023-01-18 19:10:28 +0530

    Add files via upload

---
+++ Set Operations.py
@@ -0,0 +1,14 @@
+def SrtOperation(E, N):
+    print("Union of E and N is ", E | N)
+
+    print("Intersection of E and N is ", E & N)
+
+    print("Difference of E and N is ", E - N)
+
+    print("Symmetric difference of E and N is ", E ^ N)
+
+
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+
+SrtOperation(E, N)
